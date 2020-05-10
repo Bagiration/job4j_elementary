7f8f4323932078fe1af6461c5a0ae5f562728647
Bagiration <Bagiration@mail.ru>
2020-05-10 14:43:20 +0300

    6. Идеальный вес. [#285294]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(double height) {
+        double rsl = (height-100)*1.15;
+        return rsl;
+    }
+
+
+    public static double womanWeight(double height) {
+        double rsl = (height-110)*1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double man = Fit.manWeight(170);
+        double woman = Fit.womanWeight(160);
+        System.out.println("The ideal weight of a man is " + man + "kg");
+        System.out.println("The ideal weight of a woman is " + woman + "kg" );
+        System.out.println("The ideal weight formula of a man is: Ideal weight = (height - 100)*1.15 ");
+        System.out.println("The ideal weight formula of a woman is: Ideal weight = (height - 110)*1.15");
+    }
+
+
+}

82003befd60bdc196639397709d791dc1f368b1b
Bagiration <Bagiration@mail.ru>
2020-05-11 23:57:23 +0300

    2. Тесты для идеальный вес [#285330]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -20,7 +20,20 @@ public class Fit {
         System.out.println("The ideal weight of a woman is " + woman + "kg");
         System.out.println("The ideal weight formula of a man is: Ideal weight = (height - 100)*1.15 ");
         System.out.println("The ideal weight formula of a woman is: Ideal weight = (height - 110)*1.15");
+
+        // Тест кода веса мужчины
+        double inMan = 170;
+        double expectedMan = 80.5;
+        double outMan = manWeight(inMan);
+        boolean passedMansWeight = expectedMan == outMan;
+        System.out.println("The man's weight is " + inMan + " kg when his height is " + expectedMan + ". Test result:  " + passedMansWeight);
+
+        // Тест кода веса женщины
+
+        double inwoman = 160;
+        double expectedwoman = 57.4999999;
+        double outwoman = womanWeight(inwoman);
+        boolean passedWomansWeight = expectedwoman == outwoman;
+        System.out.println("The man's weight is " + inwoman + " kg when his height is " + expectedwoman + ". Test result:  " + passedWomansWeight);
     }
-
-
 }
---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+
+// 2. Тесты для идеальный вес [#285330]
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import ru.job4j.converter.Converter;
+
+public class FitTest {
+
+    @Test
+    public void whenHeight170ThenMansWeight80Dot5() {
+        double in = 170;
+        double expected = 80.5;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+
+
+    }
+
+    @Test
+    public void whenHeight160ThenWomansWeights57Dot5() {
+        double in = 160;
+        double expected = 57.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}

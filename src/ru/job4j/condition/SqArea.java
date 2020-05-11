5bbfc1c88416c5c834e7359de6526ae27ece4adb
Bagiration <Bagiration@mail.ru>
2020-05-12 01:18:43 +0300

    4. Тесты для стороны прямоугольника. [#285332]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -12,11 +12,17 @@ public class SqArea {
         int p = 6;
         int k = 2;
         double result1 = SqArea.square(p, k);
-        System.out.println(" Square with parameters " +  p + " and " +  k + " equal is " + result1);
+        System.out.println(" Square with parameters " + p + " and " + k + " equal is " + result1);
         System.out.println(" The formula for calculate square  is S=(p/(2*(k+1))^2)*k");
 
+        // Тест кода
+
+        int inp = 6;
+        int ink = 2;
+        int expected = 2;
+        double out = square(inp, ink);
+        boolean passed = expected == out;
+        System.out.println("The area with parameters " + inp + " and " + ink + " is " + expected + ". Test result: " + passed);
+
     }
-
-
-
 }
---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class SqAreaTest {
+
+    @Test
+    public void controlArea() {
+        int inp = 6;
+        int ink = 2;
+        int expected = 2;
+        double out = SqArea.square(inp, ink);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}

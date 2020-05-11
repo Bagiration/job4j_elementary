5925c0272356dacc474e6070fc827d0167b5df56
Bagiration <Bagiration@mail.ru>
2020-05-11 13:15:40 +0300

    0. Что такое тестирование. [#285328]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -18,7 +18,19 @@ public class Converter {
         int dollar = Converter.rubleToDollar(b);
         System.out.println(a + " rubles are " + euro + " euro.");
         System.out.println(b + " rubles are " + dollar + " dollar.");
+
+        // Тест кода евро
+        int inEuro = 140;
+        int expectedEuro = 2;
+        int outEuro = rubleToEuro(inEuro);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println(inEuro + " rubles are " + expectedEuro + ". Test result:  " + passedEuro);
+
+        // Тест кода бакс
+        int inDollar = 120;
+        int expectedDollar = 2;
+        int outDollar = rubleToDollar(inDollar);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println(inDollar + " rubles are " + expectedDollar + ". Test result:  " + passedDollar);
     }
-
-
 }

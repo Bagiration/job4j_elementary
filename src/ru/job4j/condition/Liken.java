d64108560427731dee7f3a2932b3f5b694f5db45
Bagiration <Bagiration@mail.ru>
2020-05-14 00:19:12 +0300

    0. Операции сравнения [#285259]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+//0. Операции сравнения [#285259]
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+        boolean result1 = first > second;
+        boolean result2 = first < second;
+        boolean result3 = first == second;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}

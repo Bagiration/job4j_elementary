83e0638dbadc5611bf00005bafb8d3d7c2a59cd9
Bagiration <Bagiration@mail.ru>
2020-05-17 20:11:50 +0300

    //5.2. Создать программу, вычисляющую факториал. [#285352]

---
+++ src/ru/job4j/loop/Factorial.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+//5.2. Создать программу, вычисляющую факториал. [#285352]
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 1;
+        for (int index = 1; index <= n; index++) {
+            //result *= index;
+            result = result * index;
+        }
+        return result;
+    }
+
+   /* public static void main(String[] args) {
+        System.out.println(calc(0));
+    }*/
+}

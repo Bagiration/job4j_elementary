914a2b38a54a0f66c7f33f9260ba61ff71072d72
Bagiration <Bagiration@mail.ru>
2020-05-17 05:45:39 +0300

    5.0. Цикл for. Определение. [#285353]

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Bagrat Paronyan");
-        System.out.println("02.01.1984");
-    }
-}

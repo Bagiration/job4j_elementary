3c375277397d304f75ae69068ddcfeea54aa89cc
Bagiration <Bagiration@mail.ru>
2020-05-24 00:44:59 +0300

    6.0.2. Заполнение массива.

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,8 +4,17 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
- short[] ages = new short[10];
- String[] surnames = new String[100500];
- float[] prices = new float[40];
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "я";
+        names[1] = "сестра";
+        names[2] = "брат";
+        names[3] = "мама";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }

3eeba672f1f924655359fe93cab575ecd61907be
Bagiration <Bagiration@mail.ru>
2020-06-14 22:56:05 +0300

    6.7.2. Моно столбец в матрице

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 //6.7.1. Моно строка в матрице
+//6.7.2. Моно столбец в матрице
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
@@ -13,4 +14,16 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board[0].length - 1; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+//6.7.2. Моно столбец в матрице
+
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -14,6 +16,17 @@ public class MatrixCheckTest {
                 {' ', ' ', ' '},
         };
         boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
         assertThat(result, is(true));
     }
 }

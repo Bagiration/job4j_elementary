7413c8d04395e6882380936e38aa230f565ab970
Bagiration <Bagiration@mail.ru>
2020-06-16 03:52:49 +0300

    6.8. Дефрагментация массива

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -1,15 +1,19 @@
 package ru.job4j.array;
 
 public class Defragment {
+
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index; /* указатель на null ячейку. */
+                //int point = index; /* указатель на null ячейку. */
                 /* переместить первую не null ячейку. Нужен цикл. */
-                for (int i = 0; i < array.length; i++) {
-                    array[i] =
+                for (int a = index; a < array.length; a++) {
+                    if (array[a] != null) {
+                        array[index] = array[a];
+                        array[a] = null;
+                        break;
+                    }
                 }
-
             }
             System.out.print(array[index] + " ");
         }

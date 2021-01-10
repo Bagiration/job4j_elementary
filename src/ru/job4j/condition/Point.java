50155b624e0f104917b06ea95af7e8dbb167c48a
Bagiration <Bagiration@mail.ru>
2021-01-10 05:36:09 +0300

    2.1. Рефакторинг - Расстояние между точками. [#285431]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,28 +1,53 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+/**
+ * Старый код (без подхода ООП):
+ * public static double distance(int x1, int y1, int x2, int y2) {
+ * double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+ * return rsl;
+ * }
+ * <p>
+ * <p>
+ * public static void main(String[] args) {
+ * int x1 = 1;
+ * int x2 = 2;
+ * int y1 = 4;
+ * int y2 = 0;
+ * double result = Point.distance(x1, y1, x2, y2);
+ * System.out.println("distance from " + "(" + x1 + "," + y1 + ") to (" + x2 + "," + y2 + ") is " + result);
+ * <p>
+ * // Тест кода проверки расстояния
+ * int inx1 = 1;
+ * int iny1 = 4;
+ * int inx2 = 2;
+ * int iny2 = 0;
+ * double expected = 4.123105;
+ * double out = distance(inx1, iny1, inx2, iny2);
+ * boolean passed = expected == out;
+ * System.out.println("The distance between two points with coordinates " + "(" + inx1 + "," + iny1 + "), (" + inx2 + "," + iny2 + ") is " + expected + ". Test result:  " + passed);
+ * }
+ */
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rsl;
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        x = first;
+        y = second;
     }
 
+    public double distance(Point that) {
+        return sqrt(pow(x - that.x, 2) + pow(y - that.y, 2));
+    }
 
     public static void main(String[] args) {
-        int x1 = 1;
-        int x2 = 2;
-        int y1 = 4;
-        int y2 = 0;
-        double result = Point.distance(x1, y1, x2, y2);
-        System.out.println("distance from " + "(" + x1 + "," + y1 + ") to (" + x2 + "," + y2 + ") is " + result);
-
-        // Тест кода проверки расстояния
-        int inx1 = 1;
-        int iny1 = 4;
-        int inx2 = 2;
-        int iny2 = 0;
-        double expected = 4.123105;
-        double out = distance(inx1, iny1, inx2, iny2);
-        boolean passed = expected == out;
-        System.out.println("The distance between two points with coordinates " + "(" + inx1 + "," + iny1 + "), (" + inx2 + "," + iny2 + ") is " + expected + ". Test result:  " + passed);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -4,18 +4,25 @@ import org.junit.Test;
 
 import org.junit.Assert;
 
+/**
+ * старый тест:
+ * public void controlDistance() {
+ * Point a = new Point(1, 4);
+ * int inx1 = 1;
+ * int iny1 = 4;
+ * int inx2 = 2;
+ * int iny2 = 0;
+ * double expected = 4.123;
+ * double out = Point.distance(inx1, iny1, inx2, iny2);
+ */
 public class PointTest {
 
     @Test
     public void controlDistance() {
-        int inx1 = 1;
-        int iny1 = 4;
-        int inx2 = 2;
-        int iny2 = 0;
+        Point a = new Point(1, 4);
+        Point b = new Point(2, 0);
         double expected = 4.123;
-        double out = Point.distance(inx1, iny1, inx2, iny2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
-
-
     }
 }
